e1794f3cd1e291f8b6c98d16c3cdca7a65c38bb5
Wilker <wilker_a@hotmail.com>
2019-06-25 16:38:24 -0300

    gerado o Model Question e realizados o relacionamento com o Model User

--- app/User.php
+++ app/User.php
@@ -36,4 +36,15 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function questions()
+    {
+        return $this->hasMany(Question::class);
+    }
+
+    public function setTitleAttribute($value)
+    {
+        $this->attrributes['title'] = $value;
+        $this->attrributes['slug'] = str_slug($value);
+    }
 }
